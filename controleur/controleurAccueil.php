48977015b2c3736114707cc9cb9db9896a7deacb
Cyrielle-slm <slm.cyrielle@gmail.com>
2017-08-08 14:43:11 +0200

    Classe Vue modif
    Amélioration passage de 3 classe "Vue" à 1 seule.

--- controleur/controleurAccueil.php
+++ controleur/controleurAccueil.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'modele/billet.php';
-require_once 'vue/vueAcc.php';
+require_once 'vue/vue.php';
 
 class ControleurAccueil {
 
@@ -18,12 +18,12 @@ class ControleurAccueil {
 
 /*
 * Retourne tous les billets du blog
-* Instancie la classee VueAcc et prend en paramètre l'action qui dertermine le fichier vue à utiliser
+* Instancie la classee Vue et prend en paramètre l'action qui dertermine le fichier vue à utiliser et le gabarit
 *Utilisation de la fonction generer(),qui renvoi la vue (gabarit,partie spécifique)
 */
     public function accueil(){
         $listebillets = $this->billet->getBilletsAcc();
-        $vueAcc = new VueAcc( "Accueil");
+        $vueAcc = new Vue( "Accueil","Admin");
         $vueAcc->generer(array('billets'=>$listebillets));
     }
 }
