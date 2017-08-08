ee75b7e535dc183081472391a6e50778a29c1d60
Cyrielle-slm <slm.cyrielle@gmail.com>
2017-08-08 14:48:39 +0200

    Modif classe Vue
    Amélioration passage de 3 classe "Vue" à 1 seule.

--- controleur/controleurListeBillet.php
+++ controleur/controleurListeBillet.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'modele/billet.php';
-require_once 'vue/vueAdmin.php';
+require_once 'vue/vue.php';
 require_once 'modele/commentaire.php';
 
 class ControleurListeBillet {
@@ -16,12 +16,12 @@ class ControleurListeBillet {
 
 /*
 *Renvoi la liste des billets du blog (getBillets())
-* Instancie la classee VueAdmin et prend en paramètre l'action qui dertermine le fichier vue à utiliser
+* Instancie la classee Vue et prend en paramètre l'action qui dertermine le fichier vue à utiliser
 *Utilisation de la fonction generer(),qui renvoi la vue (gabarit,partie spécifique)
 */
     public function ListeBillet(){
         $listebillets = $this->billet->getBillets();
-        $vueAdmin = new VueAdmin( "ListeBillet");
+        $vueAdmin = new Vue( "ListeBillet","Admin");
         $vueAdmin->generer(array('billets'=>$listebillets));
     
 }
